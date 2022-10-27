87bf69862fc1c90584d2d727b7b09ba81e407bd8
Deo Joan <deomitokpe@gmail.com>
2022-10-27 02:26:39 -0700

    task 0

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+char *_strcat (char *dest, char *src);
+char *strncat(char *dest, char *src, int n);
+char *strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char string_toupper(char *a);
+char cap_string(char *a);
+char leet(char *a);
+int _putchar(char c);
+
+
+#endif
+
